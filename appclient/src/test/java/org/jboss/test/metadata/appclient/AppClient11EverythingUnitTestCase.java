c8d76d531568e58244e7a8d0bcbe21a6a3532327
Yeray Borges <yborgess@redhat.com>
2025-07-02 15:32:15 +0100

    [JBMETA-458] Add Application Client metadata for Jakarta EE 11Jira issue: https://issues.redhat.com/browse/JBMETA-458

---
+++ appclient/src/test/java/org/jboss/test/metadata/appclient/AppClient11EverythingUnitTestCase.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright The JBoss Metadata Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package org.jboss.test.metadata.appclient;
+
+import org.jboss.metadata.appclient.parser.spec.ApplicationClientMetaDataParser;
+import org.jboss.metadata.appclient.spec.AppClientEnvironmentRefsGroupMetaData;
+import org.jboss.metadata.appclient.spec.ApplicationClientMetaData;
+import org.jboss.metadata.merge.javaee.spec.JavaEEVersion;
+import org.jboss.test.metadata.javaee.AbstractJavaEEEverythingTest;
+
+/**
+ * App Client 11 everything test.
+ *
+ * @author Eduardo Martins
+ *
+ */
+public class AppClient11EverythingUnitTestCase extends AbstractJavaEEEverythingTest {
+
+    protected ApplicationClientMetaData unmarshal() throws Exception {
+        return ApplicationClientMetaDataParser.INSTANCE.parse(getReader());
+    }
+
+    public void testEverything() throws Exception {
+        // enableTrace("org.jboss.xb");
+        ApplicationClientMetaData result = unmarshal();
+        String prefix = "app";
+        assertEquals("11", result.getVersion());
+        assertEquals(prefix, result.getId());
+        assertTrue(result.isMetadataComplete());
+        assertEquals(prefix + "ModuleName", result.getModuleName());
+        assertDescriptionGroup(prefix, result.getDescriptionGroup());
+        assertEnvironment(prefix, result.getEnvironmentRefsGroupMetaData());
+    }
+
+    protected void assertEnvironment(String prefix, AppClientEnvironmentRefsGroupMetaData env) {
+        assertNotNull(env);
+        final Mode mode = Mode.SPEC;
+        final JavaEEVersion javaEEVersion = JavaEEVersion.V11;
+        assertRemoteEnvironment(prefix, env, true, Descriptor.APPLICATION_CLIENT, mode, javaEEVersion);
+        assertMessageDestinations(2,env.getMessageDestinations(),  mode, javaEEVersion);
+    }
+
+}
--- common/src/main/java/org/jboss/metadata/parser/util/XMLResourceResolver.java
+++ common/src/main/java/org/jboss/metadata/parser/util/XMLResourceResolver.java
@@ -99,6 +99,7 @@ public class XMLResourceResolver implements XMLResolver, EntityResolver, LSResou
         registerEntity("http://xmlns.jcp.org/xml/ns/javaee/application-client_8.xsd", "application-client_8.xsd");
         registerEntity("https://jakarta.ee/xml/ns/jakartaee/application-client_9.xsd", "application-client_9.xsd");
         registerEntity("https://jakarta.ee/xml/ns/jakartaee/application-client_10.xsd", "application-client_10.xsd");
+        registerEntity("https://jakarta.ee/xml/ns/jakartaee/application-client_11.xsd", "application-client_11.xsd");
 
         // jboss-client
         registerEntity("-//JBoss//DTD Application Client 3.2//EN", "jboss-client_3_2.dtd");
