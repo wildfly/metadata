d16f29cb1f977ea5be702800788abf9c5e6f9a38
Brian Stansberry <bstansbe@ibm.com>
2025-12-22 20:03:09 -0600

    [JBMETA-459] Add the version enums for Servlet 6.1, jboss-web_15_1.xsd schema and related parsing tests

---
+++ web/src/test/java/org/jboss/test/metadata/web/WebApp11EverythingUnitTestCase.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright The JBoss Metadata Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package org.jboss.test.metadata.web;
+
+import java.util.List;
+
+import org.jboss.metadata.merge.javaee.spec.JavaEEVersion;
+import org.jboss.metadata.web.spec.AttributeValueMetaData;
+import org.jboss.metadata.web.spec.WebMetaData;
+
+/**
+ * Test all entries of Jakarta EE 11 web-app.
+ *
+ * @author Eduardo Martins
+ *
+ */
+public class WebApp11EverythingUnitTestCase extends WebApp6EverythingUnitTestCase {
+
+    public void testEverything() throws Exception {
+        WebMetaData webApp = unmarshal();
+        assertEverything(webApp, Mode.SPEC, JavaEEVersion.V11);
+    }
+
+    protected void assertCookieAttributes(List<AttributeValueMetaData> attributes) {
+        assertNotNull("cookie attributes not set", attributes);
+        assertEquals(2, attributes.size());
+        assertEquals("SameSite", attributes.get(0).getAttributeName());
+        assertEquals("None", attributes.get(0).getAttributeValue());
+        assertEquals("foo", attributes.get(1).getAttributeName());
+        assertEquals("bar", attributes.get(1).getAttributeValue());
+    }
+}
